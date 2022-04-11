66000353e35bcece453d7da118d013a744e03db6
Archer Wayne <archerwayne1216@gmail.com>
2022-04-11 08:55:37 +0800

    update

--- collision-ball.py
+++ collision-ball.py
@@ -1,3 +1,71 @@
 import pygame, sys, time
 from debug import debug
 
+class Blocks(pygame.sprite.Sprite):
+    def __init__(self, pos, color, groups):
+        super(Blocks, self).__init__()
+        self.pos = pos
+        self.image = pygame.Surface((30, 80))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center=pos)
+        self.old_rect = self.rect.copy()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups, ball):
+        super(Player, self).__init__()
+
+        # image
+
+        # position
+
+        # movement
+
+    def input(self):
+        pass
+
+    def collision(self):
+        pass
+
+    def boundary(self):
+        pass
+
+    def update(self):
+        pass
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, groups, blocks, player):
+        super(Ball, self).__init__()
+        self.image = pygame.Surface((30, 30))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center=())
+
+    def collision(self):
+        pass
+
+    def windows_collision(self):
+        pass
+
+    def update(self):
+        pass
+
+# general setup
+pygame.init()
+screen = pygame.display.set_mode((600, 720))
+
+# group setup
+last_time = time.time()
+while True:
+
+    # delta time
+    dt = time.time() - last_time
+    last_time = time.time()
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE):
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('#123456')
+
+    pygame.display.update()
--- todo.md
+++ todo.md
@@ -5,3 +5,9 @@
    1. 不能直接用pos和rect更新, 要分别使用pos.x和pos.y
 3. player和ball之间没有碰撞, 怎么回事?
    1. 完成 要将player的位置也传给ball, 并且在碰撞检测中增加player的rect
+
+
+# collision balls to do list
+1. 这个需要delta time
+2. 用drawio画个概念图
+3. groups?
